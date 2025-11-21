6fd724c1f1d91409363318daaabb86006608494d
Do Tien Loc <doloc240305@gmail.com>
2025-11-21 12:42:32 +0700

    Lưu thay đổi header.html trước khi pull

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 // ⚙️ Cấu hình database
 const config = {
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "sa",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "123456",
   database: process.env.DB_NAME || "bookstore",
   port: process.env.DB_PORT || 3306,
